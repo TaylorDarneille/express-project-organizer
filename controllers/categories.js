481eb383a166c2e7404f245ce0cd2599629db648
nichro02 <nichro02@gmail.com>
2020-11-01 14:12:29 -0800

    Added categories folder within views to display categories and generated a page to display all categories within database

--- controllers/categories.js
+++ controllers/categories.js
@@ -5,5 +5,24 @@ let router = express.Router()
 // GET /categories
 //Show all categories that exist
 
+router.get('/', (req, res) => {
+    db.category.findAll()
+    .then((categories) => {
+        console.log(categories)
+        res.render('categories/index', { categories: categories })
+    })
+    .catch((error) => {
+    console.log('Error in GET /', error)
+    res.status(400).render('main/404')
+    })
+    //res.send('LIST OF CATEGORIES')
+})
+
 // GET /categories/:id
 //Show a specific category and all projects associated with that category
+
+router.get('/:id', (req, res) => {
+    res.send('SHOW SPECIFIC CATEGORY')
+})
+
+module.exports = router
