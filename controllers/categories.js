73ac031796a8e2f41309eb7be9bd63eddbc4c3ff
epizarro98 <epizarro309@gmail.com>
2020-11-03 23:15:07 -0500

    finished hw

--- controllers/categories.js
+++ controllers/categories.js
@@ -14,18 +14,31 @@ router.get('/', (req, res)=>{
 })
 
 router.get('/:id', (req, res)=>{
+    // res.send('hellooooooooo')
+    console.log(req.params.id)
     db.category.findOne({
         where: {id: req.params.id},
         include: [db.project]
     })
     .then((category)=>{
+        console.log(category)
         //render category show page, pass in category to res.render in the show page
         if (!category) throw Error()
-        res.render('categories/show', {category})
+        res.render('categories/show', {category: category, projects: category.projects})
+        // res.send('hello')
     })
     .catch(err =>{
         console.log('THIS IS THE ERROR<================', err)
     })
+    // db.category.findByPk(req.params.id, {
+    //     include: [db.project]
+    // })
+    // .then(foundCategory =>{
+    //     console.log('FOUND CATEGORIES<<<<<<<<<<', foundCategory.projects)
+    // })
+    // .catch(err =>{
+    //     console.log('ERROR <=========', err)
+    // })
 })
 
 
