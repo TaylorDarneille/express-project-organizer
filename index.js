a0ffb1228e8227cfa090b28b0e10fd71cc310aa6
xlnl <lamanhnle@gmail.com>
2020-11-03 19:54:27 -0500

    debugged! just forgot to add closing ejs tags

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/projects', require('./controllers/projects'))
-app.use('/projects', require('./controllers/categories'))
+app.use('/categories', require('./controllers/categories'))
 
 app.get('*', (req, res) => {
   res.render('main/404')
