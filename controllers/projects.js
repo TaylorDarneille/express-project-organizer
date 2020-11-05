6e6f8fa97145903073900480fe1eb0742e2c18f0
granddivino <granddivino@gmail.com>
2020-11-04 17:28:58 -0700

    Gabriel G. - Mateen - Express Project Organizer

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let db = require('../models')
+const project = require('../models/project')
 let router = express.Router()
 
 // POST /projects - create a new project
@@ -11,30 +12,35 @@ router.post('/', (req, res) => {
     description: req.body.description
   })
   .then((project) => {
-    res.redirect('/')
+    db.category.findOrCreate({
+      where: {name: req.body.category}
+    })
+    .then(([category, wasCreated]) => {
+        project.addCategory(category)
+        res.redirect('/')
+    })
+    .catch((error) => {
+      res.status(400).render('main/404')
+      console.log(error)
+    })
   })
-  .catch((error) => {
-    res.status(400).render('main/404')
   })
-})
-
-// GET /projects/new - display form for creating a new project
-router.get('/new', (req, res) => {
-  res.render('projects/new')
-})
-
-// GET /projects/:id - display a specific project
-router.get('/:id', (req, res) => {
-  db.project.findOne({
-    where: { id: req.params.id }
+  
+  // GET /projects/new - display form for creating a new project
+  router.get('/new', (req, res) => {
+    res.render('projects/new')
   })
-  .then((project) => {
-    if (!project) throw Error()
-    res.render('projects/show', { project: project })
+  // GET /projects/:id - display a specific project
+  router.get('/:id', (req, res) => {
+    db.project.findOne({
+      where: {id: req.params.id}
+    })
+    .then((project) => {
+      if (!project) throw Error()
+      res.render('projects/show', {project: project})
+    })
+    .catch((error) => {
+      res.status(400).render('main/404')
+    })
   })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
-})
-
-module.exports = router
+  module.exports = router
