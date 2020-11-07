fca3450adc3d41f8230cb0dbb0cede3285aee529
sethheff <seth@heffnerdesigninc.com>
2020-11-07 14:14:18 -0600

    was able to fix the categories-tab issue

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let express = require('express')
 let ejsLayouts = require('express-ejs-layouts')
 let db = require('./models')
 let app = express()
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
